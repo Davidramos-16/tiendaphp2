d9038c7b392a143ec67c17c22d5d3dd5651a0381
David Ramos <dr264289@gmail.com>
2023-10-26 16:06:58 -0600

    CORS added to controller

--- controller/registrarUsuarioController.php
+++ controller/registrarUsuarioController.php
@@ -1,5 +1,8 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 require_once('../controller/usuarioController.php');
 require_once('../model/usuario.php');
 
--- controller/usuarioController.php
+++ controller/usuarioController.php
@@ -3,6 +3,10 @@ require_once('../config/database.php');
 require_once('../view/login.html');
 require_once('../model/usuario.php');
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
    if($_POST)
    {
     $usuario = new usuarioController();
